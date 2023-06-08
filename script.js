432067d97c1841430123bc297392fd998dc491be
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-06-08 09:21:57 -0500

    add js

---
+++ script.js
@@ -0,0 +1,11 @@
+let projects = []
+
+//function to render the projects
+function renderProjects() {
+    const projectList = document.getElementById('project-list');
+    projectList.innerHTML = '';
+
+    projects.forEach((project) => {
+        const projectItem = document.createElement('li');
+    })
+}
