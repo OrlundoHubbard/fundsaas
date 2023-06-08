1e4e37f34b930778e289e42adec39e707024c95b
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-06-08 09:44:31 -0500

    more js

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ function renderProjects() {
 
     projects.forEach((project) => {
         const projectItem = document.createElement('li');
+        projectItem.className = 'project mb-4 p-4 bg-white rounded shadow';
+        projectItem.innerHTML = `<h3 class="text-xl font-bold mb-2>${project.name}</h3>
+        <p class="text-gray-700 mb-2">${project.description}
+        <p class="text-gray-600>Goal: $${project.goal} `
     })
 }
